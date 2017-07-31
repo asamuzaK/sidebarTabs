6b25238ffef58b6ac24bb57f9e1e90d13f5cd790
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-07-31 20:19:11 +0900

    Refactor twitter workaround

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -9,23 +9,47 @@
 
   /* constant */
   const TAB_OBSERVE = "observeTab";
+  const TIME_3SEC = 3000;
 
   /**
-   * send message when new tweets bar is clicked
-   * @param {!Object} evt - Event
-   * @returns {void}
+   * throw error
+   * @param {!Object} e - Error
+   * @throws - Error
    */
-  const sendObserveTabMsg = evt => {
+  const throwErr = e => {
+    throw e;
+  };
+
+  /**
+   * send message
+   * @param {*} msg - message
+   * @returns {?AsyncFunction} - runtime.sendMessage()
+   */
+  const sendMsg = async msg => {
+    const func = msg && runtime.sendMessage(msg);
+    return func || null;
+  };
+
+  window.addEventListener("click", evt => {
+    console.log(evt.type);
     const {target} = evt;
     const {parentNode} = target;
     const reg = /js-n(?:av|ew-tweets-bar)/;
-    if (reg.test(target.className) ||
-        parentNode && reg.test(parentNode.className)) {
-      runtime.sendMessage({
-        [TAB_OBSERVE]: true,
-      });
-    }
-  };
+    const func = (reg.test(target) || reg.test(parentNode)) && sendMsg({
+      [TAB_OBSERVE]: true,
+    }).catch(throwErr);
+    return func || null;
+  });
 
-  window.addEventListener("click", sendObserveTabMsg);
+  window.addEventListener("load", () => {
+    const func = [
+      sendMsg({
+        [TAB_OBSERVE]: true,
+      }),
+      setTimeout(() => sendMsg({
+        [TAB_OBSERVE]: true,
+      }).catch(throwErr), TIME_3SEC)
+    ];
+    return Promise.all(func).catch(throwErr);
+  });
 }
