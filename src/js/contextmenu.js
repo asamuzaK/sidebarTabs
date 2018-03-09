38a4621ac6953f1d13be159fcb83d894ea75ddfb
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-03-09 15:39:58 +0900

    Refactor script

--- src/js/contextmenu.js
+++ src/js/contextmenu.js
@@ -66,24 +66,6 @@
   };
 
   /**
-   * given element is context menu item
-   * @param {Object} elm - element
-   * @returns {boolean} - result
-   */
-  const isContextMenuItem = elm => {
-    let bool = false, node = elm;
-    while (node && node.parentNode) {
-      const {classList} = node;
-      bool = classList.contains(CLASS_MENU);
-      if (bool) {
-        break;
-      }
-      node = node.parentNode;
-    }
-    return bool;
-  };
-
-  /**
    * remove style
    * @param {Object} elm - element
    * @returns {void}
@@ -101,157 +83,42 @@
   };
 
   /**
-   * hide sub menus
-   * @param {!Object} evt - Event
-   * @returns {void}
+   * get offsets
+   * @param {Object} elm - Element
+   * @retuns {Object} - offsets
    */
-  const hideSubMenus = evt => {
-    const {target} = evt;
-    const {id: targetId, parentNode: {childNodes}} = target;
-    if (targetId === MENU) {
-      const subMenus = target.querySelectorAll(`.${CLASS_MENU}`);
-      for (const elm of subMenus) {
-        removeStyle(elm);
-      }
-    } else {
-      const arr = [];
-      for (const child of childNodes) {
-        if (child.nodeType === Node.ELEMENT_NODE && child !== target) {
-          const {classList: childClassList} = child;
-          if (childClassList.contains(CLASS_SUBMENU_CONTAINER)) {
-            arr.push(child);
-          }
-        }
-      }
-      if (arr.length) {
-        arr.forEach(elm => {
-          const subMenus = elm.querySelectorAll(`.${CLASS_MENU}`);
-          for (const item of subMenus) {
-            removeStyle(item);
-          }
-        });
-      }
+  const getOffsets = elm => {
+    const offset = {
+      bottom: 0,
+      height: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 0,
+    };
+    if (elm && elm.nodeType === Node.ELEMENT_NODE) {
+      const top =
+        window.getComputedStyle(elm).marginTop.replace("px", "") * 1 +
+        window.getComputedStyle(elm).borderTopWidth.replace("px", "") * 1;
+      const right =
+        window.getComputedStyle(elm).marginRight.replace("px", "") * 1 +
+        window.getComputedStyle(elm).borderRightWidth.replace("px", "") * 1;
+      const bottom =
+        window.getComputedStyle(elm).marginBottom.replace("px", "") * 1 +
+        window.getComputedStyle(elm).borderBottomWidth.replace("px", "") * 1;
+      const left =
+        window.getComputedStyle(elm).marginLeft.replace("px", "") * 1 +
+        window.getComputedStyle(elm).borderLeftWidth.replace("px", "") * 1;
+      const width = left + right;
+      const height = top + bottom;
+      offset.top = top;
+      offset.right = right;
+      offset.bottom = bottom;
+      offset.left = left;
+      offset.width = width;
+      offset.height = height;
     }
-  };
-
-  /**
-   * show sub menu
-   * @param {!Object} evt - Event
-   * @returns {Object} - sub menu element
-   */
-  const showSubMenu = evt => {
-    const {target} = evt;
-    const {childNodes} = target;
-    let elm;
-    for (const item of childNodes) {
-      if (item.nodeType === Node.ELEMENT_NODE &&
-          item.classList.contains(CLASS_MENU)) {
-        elm = item;
-        break;
-      }
-    }
-    if (elm) {
-      const {classList: elmClassList, style: elmStyle} = elm;
-      elmClassList.contains(CLASS_MENU) && elmClassList.add(CLASS_SHOW);
-      if (elmClassList.contains(CLASS_SHOW)) {
-        const {innerHeight, innerWidth} = window;
-        const {
-          bottom: targetBottom, left: targetLeft, height: targetHeight,
-          right: targetRight, top: targetTop, width: targetWidth,
-        } = target.getBoundingClientRect();
-        const {
-          height: elmHeight, width: elmWidth,
-        } = elm.getBoundingClientRect();
-        const rem =
-          window.getComputedStyle(document.documentElement).fontSize
-            .replace("px", "") * 1;
-        const offsetTop =
-          window.getComputedStyle(elm).marginTop.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderTopWidth.replace("px", "") * 1;
-        const offsetRight =
-          window.getComputedStyle(elm).marginRight.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderRightWidth.replace("px", "") * 1;
-        const offsetBottom =
-          window.getComputedStyle(elm).marginBottom.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderBottomWidth.replace("px", "") * 1;
-        const offsetLeft =
-          window.getComputedStyle(elm).marginLeft.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderLeftWidth.replace("px", "") * 1;
-        const offsetWidth = offsetLeft + offsetRight;
-        const offsetHeight = offsetTop + offsetBottom;
-        if (innerWidth > targetRight + elmWidth + offsetWidth) {
-          // show right
-          elmStyle.left = `${targetWidth - offsetRight}px`;
-          if (innerHeight > targetTop + elmHeight + offsetHeight) {
-            // show downwards
-            elmStyle.top = `-${offsetTop}px`;
-          } else {
-            // show upwards
-            elmStyle.top = `${targetHeight - elmHeight}px`;
-          }
-        } else if (targetLeft > elmWidth + offsetWidth) {
-          // show left
-          elmStyle.left = `-${elmWidth}px`;
-          if (innerHeight > targetTop + elmHeight + offsetHeight) {
-            // show downwards
-            elmStyle.top = `-${offsetTop}px`;
-          } else {
-            // show upwards
-            elmStyle.top = `${targetHeight - elmHeight}px`;
-          }
-        } else if (innerWidth - targetRight > targetLeft) {
-          if (elmWidth + offsetWidth > innerWidth - targetRight - rem) {
-            // fit right edge of the page
-            elmStyle.left = `${innerWidth - targetRight - offsetRight}px`;
-          } else {
-            // offset right
-            elmStyle.left = `${targetRight - rem - offsetLeft}px`;
-          }
-          if (innerHeight > targetBottom + elmHeight + offsetBottom) {
-            // offset below
-            elmStyle.top = `${targetHeight - offsetBottom}px`;
-          } else {
-            // offset above
-            elmStyle.top = `-${elmHeight}px`;
-          }
-        } else {
-          if (elmWidth + offsetWidth > targetLeft + rem) {
-            // fit left edge
-            elmStyle.left = `${offsetLeft - targetLeft}px`;
-          } else {
-            // offset left
-            elmStyle.left = `${rem + offsetRight - elmWidth}px`;
-          }
-          if (innerHeight > targetBottom + elmHeight + offsetBottom) {
-            // offset below
-            elmStyle.top = `${targetHeight - offsetBottom}px`;
-          } else {
-            // offset above
-            elmStyle.top = `-${elmHeight}px`;
-          }
-        }
-        elmClassList.add(CLASS_VISIBLE);
-      }
-    }
-    return elm || null;
-  };
-
-  /**
-   * hide context menu
-   * @returns {Object} - context menu element
-   */
-  const hideContextMenu = () => {
-    const elm = document.getElementById(MENU);
-    const container = document.getElementById(MENU_CONTAINER);
-    if (elm) {
-      const subMenus = elm.querySelectorAll(`.${CLASS_MENU}`);
-      for (const item of subMenus) {
-        removeStyle(item);
-      }
-      removeStyle(elm);
-    }
-    container && container.classList.remove(CLASS_SHOW);
-    return elm || null;
+    return offset;
   };
 
   /**
@@ -265,6 +132,26 @@
       target.focus();
     }
     return target || null;
+  };
+
+  /**
+   * given element is a context menu item
+   * @param {Object} elm - Element
+   * @returns {boolean} - result
+   */
+  const isContextMenuItem = elm => {
+    let bool;
+    if (elm && elm.nodeType === Node.ELEMENT_NODE) {
+      let node = elm;
+      while (node && node.parentNode) {
+        bool = node.classList.contains(CLASS_MENU);
+        if (bool) {
+          break;
+        }
+        node = node.parentNode;
+      }
+    }
+    return !!bool;
   };
 
   /**
@@ -416,17 +303,183 @@
   };
 
   /**
+   * hide sub menus
+   * @param {!Object} evt - Event
+   * @returns {void}
+   */
+  const hideSubMenus = evt => {
+    const {target} = evt;
+    const {id: targetId, parentNode: {childNodes}} = target;
+    if (targetId === MENU) {
+      const subMenus = target.querySelectorAll(`.${CLASS_MENU}`);
+      for (const elm of subMenus) {
+        removeStyle(elm);
+      }
+    } else {
+      const arr = [];
+      for (const child of childNodes) {
+        if (child.nodeType === Node.ELEMENT_NODE && child !== target) {
+          const {classList: childClassList} = child;
+          if (childClassList.contains(CLASS_SUBMENU_CONTAINER)) {
+            arr.push(child);
+          }
+        }
+      }
+      if (arr.length) {
+        arr.forEach(elm => {
+          const subMenus = elm.querySelectorAll(`.${CLASS_MENU}`);
+          for (const item of subMenus) {
+            removeStyle(item);
+          }
+        });
+      }
+    }
+  };
+
+  /**
+   * show sub menu
+   * @param {!Object} evt - Event
+   * @returns {Object} - sub menu element
+   */
+  const showSubMenu = evt => {
+    const {target} = evt;
+    const {childNodes} = target;
+    let elm;
+    for (const item of childNodes) {
+      if (item.nodeType === Node.ELEMENT_NODE &&
+          item.classList.contains(CLASS_MENU)) {
+        elm = item;
+        break;
+      }
+    }
+    if (elm) {
+      const {classList: elmClassList, style: elmStyle} = elm;
+      elmClassList.contains(CLASS_MENU) && elmClassList.add(CLASS_SHOW);
+      if (elmClassList.contains(CLASS_SHOW)) {
+        const {innerHeight, innerWidth} = window;
+        const {
+          bottom: targetBottom, left: targetLeft, height: targetHeight,
+          right: targetRight, top: targetTop, width: targetWidth,
+        } = target.getBoundingClientRect();
+        const {
+          height: elmHeight, width: elmWidth,
+        } = elm.getBoundingClientRect();
+        const {
+          bottom: offsetBottom, height: offsetHeight, left: offsetLeft,
+          right: offsetRight, top: offsetTop, width: offsetWidth,
+        } = getOffsets(elm);
+        const rem =
+          window.getComputedStyle(document.documentElement).fontSize
+            .replace("px", "") * 1;
+        if (innerWidth > targetRight + elmWidth + offsetWidth) {
+          // show right
+          elmStyle.left = `${targetWidth - offsetRight}px`;
+          if (innerHeight > targetTop + elmHeight + offsetHeight) {
+            // show downward
+            elmStyle.top = `-${offsetTop}px`;
+          } else {
+            // show upward
+            elmStyle.top = `${targetHeight - elmHeight}px`;
+          }
+        } else if (targetLeft > elmWidth + offsetWidth) {
+          // show left
+          elmStyle.left = `-${elmWidth}px`;
+          if (innerHeight > targetTop + elmHeight + offsetHeight) {
+            // show downward
+            elmStyle.top = `-${offsetTop}px`;
+          } else {
+            // show upward
+            elmStyle.top = `${targetHeight - elmHeight}px`;
+          }
+        } else if (innerWidth - targetRight > targetLeft) {
+          if (elmWidth + offsetWidth > innerWidth - targetRight - rem) {
+            // fit right edge of the page
+            elmStyle.left = `${innerWidth - targetRight - offsetRight}px`;
+          } else {
+            // offset right
+            elmStyle.left = `${targetRight - rem - offsetLeft}px`;
+          }
+          if (innerHeight > targetBottom + elmHeight + offsetBottom) {
+            // offset below
+            elmStyle.top = `${targetHeight - offsetBottom}px`;
+          } else {
+            // offset above
+            elmStyle.top = `-${elmHeight}px`;
+          }
+        } else {
+          if (elmWidth + offsetWidth > targetLeft + rem) {
+            // fit left edge of the page
+            elmStyle.left = `${offsetLeft - targetLeft}px`;
+          } else {
+            // offset left
+            elmStyle.left = `${rem + offsetRight - elmWidth}px`;
+          }
+          if (innerHeight > targetBottom + elmHeight + offsetBottom) {
+            // offset below
+            elmStyle.top = `${targetHeight - offsetBottom}px`;
+          } else {
+            // offset above
+            elmStyle.top = `-${elmHeight}px`;
+          }
+        }
+        elmClassList.add(CLASS_VISIBLE);
+      }
+    }
+    return elm || null;
+  };
+
+  /**
+   * hide context menu container
+   * @returns {Object} - context menu container element
+   */
+  const hideContextMenuContainer = () => {
+    const container = document.getElementById(MENU_CONTAINER);
+    if (container) {
+      container.classList.remove(CLASS_SHOW);
+    }
+    return container || null;
+  };
+
+  /**
+   * show context menu container
+   * @returns {Object} - context menu container element
+   */
+  const showContextMenuContainer = () => {
+    const container = document.getElementById(MENU_CONTAINER);
+    if (container) {
+      container.classList.add(CLASS_SHOW);
+    }
+    return container || null;
+  };
+
+  /**
+   * hide context menu
+   * @returns {Object} - context menu element
+   */
+  const hideContextMenu = () => {
+    const elm = document.getElementById(MENU);
+    if (elm) {
+      const subMenus = elm.querySelectorAll(`.${CLASS_MENU}`);
+      for (const item of subMenus) {
+        removeStyle(item);
+      }
+      removeStyle(elm);
+    }
+    hideContextMenuContainer();
+    return elm || null;
+  };
+
+  /**
    * handle click
    * @param {!Object} evt - Event
    * @returns {?Function} - handler function
    */
   const handleClick = evt => {
     const {target} = evt;
-    const elm = document.getElementById(MENU);
     const isEnter = isContextMenuItem(target);
     let func;
-    if (elm && isEnter) {
-      dispatchKeyboardEvt(target, "keydown", {key: "Escape"});
+    if (isEnter) {
+      func = dispatchKeyboardEvt(target, "keydown", {key: "Escape"});
     }
     return func || null;
   };
@@ -438,12 +491,11 @@
    */
   const showContextMenu = evt => {
     const elm = document.getElementById(MENU);
-    const container = document.getElementById(MENU_CONTAINER);
-    if (elm && container) {
+    const container = showContextMenuContainer();
+    if (elm && container && container.classList.contains(CLASS_SHOW)) {
       const {classList: elmClassList, style: elmStyle} = elm;
       evt.stopImmediatePropagation();
       evt.preventDefault();
-      container.classList.add(CLASS_SHOW);
       hideSubMenus({target: elm});
       elmClassList.contains(CLASS_MENU) && elmClassList.add(CLASS_SHOW);
       if (elmClassList.contains(CLASS_SHOW)) {
@@ -452,20 +504,10 @@
         const {
           height: elmHeight, width: elmWidth,
         } = elm.getBoundingClientRect();
-        const offsetTop =
-          window.getComputedStyle(elm).marginTop.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderTopWidth.replace("px", "") * 1;
-        const offsetRight =
-          window.getComputedStyle(elm).marginRight.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderRightWidth.replace("px", "") * 1;
-        const offsetBottom =
-          window.getComputedStyle(elm).marginBottom.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderBottomWidth.replace("px", "") * 1;
-        const offsetLeft =
-          window.getComputedStyle(elm).marginLeft.replace("px", "") * 1 +
-          window.getComputedStyle(elm).borderLeftWidth.replace("px", "") * 1;
-        const offsetWidth = offsetLeft + offsetRight;
-        const offsetHeight = offsetTop + offsetBottom;
+        const {
+          bottom: offsetBottom, height: offsetHeight, left: offsetLeft,
+          right: offsetRight, top: offsetTop, width: offsetWidth,
+        } = getOffsets(elm);
         const menuItems = elm.querySelectorAll(`li:not(.${CLASS_MENU_SEP})`);
         if (innerWidth > clientX + elmWidth + offsetWidth) {
           // show right
@@ -474,14 +516,14 @@
           // show left
           elmStyle.left = `${clientX - elmWidth}px`;
         } else {
-          // show left edge
+          // show left edge of the page
           elmStyle.left = `${offsetLeft}px`;
         }
         if (innerHeight > clientY + elmHeight + offsetHeight) {
-          // show downwards
+          // show downward
           elmStyle.top = `${clientY}px`;
         } else {
-          // show upwards
+          // show upward
           elmStyle.top = `${clientY - elmHeight}px`;
         }
         for (const item of menuItems) {
@@ -510,12 +552,11 @@
     const {key, shiftKey, target} = evt;
     let func;
     if (key === "ContextMenu" || shiftKey && key === "F10") {
-      const container = document.getElementById(MENU_CONTAINER);
-      container && container.classList.add(CLASS_SHOW);
+      func = showContextMenuContainer();
     } else if (key === "Escape") {
       func = hideContextMenu();
     } else if (key === "Enter") {
-      dispatchClickEvt(target);
+      func = dispatchClickEvt(target);
     } else if (/^Arrow/.test(key)) {
       func = selectMenuItemWithArrowKey(key);
     }
@@ -531,8 +572,7 @@
     const {button, target} = evt;
     let func;
     if (button === MOUSE_BUTTON_RIGHT) {
-      const container = document.getElementById(MENU_CONTAINER);
-      container && container.classList.add(CLASS_SHOW);
+      func = showContextMenuContainer();
     } else {
       const isEsc = !isContextMenuItem(target);
       if (isEsc) {
