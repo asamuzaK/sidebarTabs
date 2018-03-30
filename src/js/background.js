f1f14d79b3facaa4ea9944ebb96e9e6109edaefb
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-03-30 14:56:44 +0900

    Toggle sidebar open/close

--- src/js/background.js
+++ src/js/background.js
@@ -4,9 +4,43 @@
 "use strict";
 {
   /* api */
-  const {browserAction, sidebarAction} = browser;
+  const {browserAction, sidebarAction, windows} = browser;
+  const {WINDOW_ID_CURRENT} = windows;
+
+  /**
+   * throw error
+   * @param {!Object} e - Error
+   * @throws - Error
+   */
+  const throwErr = e => {
+    throw e;
+  };
+
+  /* sidebar */
+  const sidebar = {
+    isOpen: false,
+  };
+
+  /**
+   * toggle sidebar state
+   * @returns {void}
+   */
+  const toggleSidebarState = async () => {
+    const isOpen = await sidebarAction.isOpen({
+      windowId: WINDOW_ID_CURRENT,
+    });
+    sidebar.isOpen = !!isOpen;
+  };
 
   browserAction.onClicked.addListener(() => {
-    sidebarAction.open();
+    const {isOpen} = sidebar;
+    const func = [];
+    if (isOpen) {
+      func.push(sidebarAction.close());
+    } else {
+      func.push(sidebarAction.open());
+    }
+    func.push(toggleSidebarState());
+    return Promise.all(func).catch(throwErr);
   });
 }
