1c77e137a47f06e1b484c4fa67dfb62dcabdaf61
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-10-05 21:14:39 +0900

    Refactor context menu based on new firefox spec
    Fix #57

--- src/mjs/contextmenu.js
+++ src/mjs/contextmenu.js
@@ -2,16 +2,19 @@
  * contextmenu.js
  */
 
-import {isString, throwErr} from "./common.js";
+import {getType, isString, throwErr} from "./common.js";
 import {getAllContextualIdentities} from "./browser.js";
 import {
-  TAB, TAB_BOOKMARK, TAB_BOOKMARK_ALL, TAB_CLOSE, TAB_CLOSE_END,
-  TAB_CLOSE_OTHER, TAB_CLOSE_UNDO, TAB_DUPE,
-  TAB_GROUP, TAB_GROUP_BOOKMARK, TAB_GROUP_CLOSE, TAB_GROUP_COLLAPSE,
-  TAB_GROUP_DETACH, TAB_GROUP_DUPE, TAB_GROUP_EXPAND, TAB_GROUP_PIN,
-  TAB_GROUP_RELOAD, TAB_GROUP_SELECTED, TAB_GROUP_SYNC, TAB_GROUP_UNGROUP,
-  TAB_MOVE_WIN_NEW, TAB_MUTE, TAB_MUTE_UNMUTE, TAB_PIN, TAB_PIN_UNPIN,
-  TAB_RELOAD, TAB_RELOAD_ALL, TAB_REOPEN_CONTAINER, TAB_SELECT_ALL, TAB_SYNC,
+  TAB_ALL_BOOKMARK, TAB_ALL_RELOAD, TAB_ALL_SELECT,
+  TAB_BOOKMARK, TAB_CLOSE, TAB_CLOSE_END, TAB_CLOSE_OPTIONS, TAB_CLOSE_OTHER,
+  TAB_CLOSE_UNDO, TAB_DUPE,
+  TAB_GROUP, TAB_GROUP_COLLAPSE, TAB_GROUP_DETACH, TAB_GROUP_EXPAND,
+  TAB_GROUP_SELECTED, TAB_GROUP_UNGROUP,
+  TAB_MOVE, TAB_MOVE_END, TAB_MOVE_START, TAB_MOVE_WIN, TAB_MUTE,
+  TAB_MUTE_UNMUTE, TAB_PIN, TAB_PIN_UNPIN, TAB_RELOAD, TAB_REOPEN_CONTAINER,
+  TABS_BOOKMARK, TABS_CLOSE, TABS_CLOSE_OTHER, TABS_MOVE, TABS_MOVE_END,
+  TABS_MOVE_START, TABS_MOVE_WIN, TABS_MUTE, TABS_MUTE_UNMUTE, TABS_PIN,
+  TABS_PIN_UNPIN, TABS_RELOAD,
 } from "./constant.js";
 
 /* api */
@@ -22,106 +25,189 @@ const ICON_SIZE_16 = "16";
 
 /* context menu items */
 export const menuItems = {
-  /* tab */
-  [TAB]: {
-    id: TAB,
-    title: i18n.getMessage(`${TAB}_title`, "(&T)"),
+  [TAB_RELOAD]: {
+    id: TAB_RELOAD,
+    title: i18n.getMessage(`${TAB_RELOAD}_title`, "(&R)"),
     type: "normal",
     enabled: false,
-    subItems: {
-      [TAB_RELOAD]: {
-        id: TAB_RELOAD,
-        title: i18n.getMessage(`${TAB_RELOAD}_title`, "(&R)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_SYNC]: {
-        id: TAB_SYNC,
-        title: i18n.getMessage(`${TAB_SYNC}_title`, "(&S)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_MUTE]: {
-        id: TAB_MUTE,
-        title: i18n.getMessage(`${TAB_MUTE}_title`, "(&M)"),
-        toggleTitle: i18n.getMessage(`${TAB_MUTE_UNMUTE}_title`, "(&M)"),
-        type: "normal",
-        enabled: false,
-      },
-      "sepTab-1": {
-        id: "sepTab-1",
-        type: "separator",
-      },
-      [TAB_PIN]: {
-        id: TAB_PIN,
-        title: i18n.getMessage(`${TAB_PIN}_title`, "(&P)"),
-        toggleTitle: i18n.getMessage(`${TAB_PIN_UNPIN}_title`, "(&P)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_DUPE]: {
-        id: TAB_DUPE,
-        title: i18n.getMessage(`${TAB_DUPE}_title`, "(&D)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_REOPEN_CONTAINER]: {
-        id: TAB_REOPEN_CONTAINER,
-        title: i18n.getMessage(`${TAB_REOPEN_CONTAINER}_title`, "(&E)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_MOVE_WIN_NEW]: {
-        id: TAB_MOVE_WIN_NEW,
-        title: i18n.getMessage(`${TAB_MOVE_WIN_NEW}_title`, "(&N)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_BOOKMARK]: {
-        id: TAB_BOOKMARK,
-        title: i18n.getMessage(`${TAB_BOOKMARK}_title`, "(&B)"),
-        type: "normal",
-        enabled: false,
-      },
-      "sepTab-2": {
-        id: "sepTab-2",
-        type: "separator",
-      },
-      // FIXME: issue #7
-      /*
-      [TAB_SEND]: {
-        id: TAB_SEND,
-        title: i18n.getMessage(`${TAB_SEND}_title`, "(&N)"),
-        type: "normal",
-        enabled: false,
-      },
-      "sepTab-3": {
-        id: "sepTab-3",
-        type: "separator",
-      },
-      */
-      [TAB_CLOSE_END]: {
-        id: TAB_CLOSE_END,
-        title: i18n.getMessage(`${TAB_CLOSE_END}_title`, "(&E)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_CLOSE_OTHER]: {
-        id: TAB_CLOSE_OTHER,
-        title: i18n.getMessage(`${TAB_CLOSE_OTHER}_title`, "(&O)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_CLOSE]: {
-        id: TAB_CLOSE,
-        title: i18n.getMessage(`${TAB_CLOSE}_title`, "(&C)"),
-        type: "normal",
-        enabled: false,
-      },
-    },
+    visible: true,
+  },
+  [TABS_RELOAD]: {
+    id: TABS_RELOAD,
+    title: i18n.getMessage(`${TABS_RELOAD}_title`, "(&R)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+  [TAB_MUTE]: {
+    id: TAB_MUTE,
+    title: i18n.getMessage(`${TAB_MUTE}_title`, "(&M)"),
+    toggleTitle: i18n.getMessage(`${TAB_MUTE_UNMUTE}_title`, "(&M)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TABS_MUTE]: {
+    id: TABS_MUTE,
+    title: i18n.getMessage(`${TABS_MUTE}_title`, "(&M)"),
+    toggleTitle: i18n.getMessage(`${TABS_MUTE_UNMUTE}_title`, "(&M)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
   },
   "sep-1": {
     id: "sep-1",
+    type: "separator",
+  },
+  [TAB_PIN]: {
+    id: TAB_PIN,
+    title: i18n.getMessage(`${TAB_PIN}_title`, "(&P)"),
+    toggleTitle: i18n.getMessage(`${TAB_PIN_UNPIN}_title`, "(&P)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TABS_PIN]: {
+    id: TABS_PIN,
+    title: i18n.getMessage(`${TABS_PIN}_title`, "(&P)"),
+    toggleTitle: i18n.getMessage(`${TABS_PIN_UNPIN}_title`, "(&P)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+  [TAB_BOOKMARK]: {
+    id: TAB_BOOKMARK,
+    title: i18n.getMessage(`${TAB_BOOKMARK}_title`, "(&B)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TABS_BOOKMARK]: {
+    id: TABS_BOOKMARK,
+    title: i18n.getMessage(`${TABS_BOOKMARK}_title`, "(&B)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+  [TAB_DUPE]: {
+    id: TAB_DUPE,
+    title: i18n.getMessage(`${TAB_DUPE}_title`, "(&D)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TAB_REOPEN_CONTAINER]: {
+    id: TAB_REOPEN_CONTAINER,
+    title: i18n.getMessage(`${TAB_REOPEN_CONTAINER}_title`, "(&E)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TAB_MOVE]: {
+    id: TAB_MOVE,
+    title: i18n.getMessage(`${TAB_MOVE}_title`, "(&V)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+    subItems: {
+      [TAB_MOVE_START]: {
+        id: TAB_MOVE_START,
+        title: i18n.getMessage(`${TAB_MOVE_START}_title`, "(&S)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      [TAB_MOVE_END]: {
+        id: TAB_MOVE_END,
+        title: i18n.getMessage(`${TAB_MOVE_END}_title`, "(&E)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      [TAB_MOVE_WIN]: {
+        id: TAB_MOVE_WIN,
+        title: i18n.getMessage(`${TAB_MOVE_WIN}_title`, "(&W)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+    },
+  },
+  [TABS_MOVE]: {
+    id: TABS_MOVE,
+    title: i18n.getMessage(`${TABS_MOVE}_title`, "(&V)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+    subItems: {
+      [TABS_MOVE_START]: {
+        id: TABS_MOVE_START,
+        title: i18n.getMessage(`${TAB_MOVE_START}_title`, "(&S)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      [TABS_MOVE_END]: {
+        id: TABS_MOVE_END,
+        title: i18n.getMessage(`${TAB_MOVE_END}_title`, "(&E)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      [TABS_MOVE_WIN]: {
+        id: TABS_MOVE_WIN,
+        title: i18n.getMessage(`${TABS_MOVE_WIN}_title`, "(&W)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+    },
+  },
+  // FIXME: issue #7
+  /*
+  [TAB_SEND]: {
+    id: TAB_SEND,
+    title: i18n.getMessage(`${TAB_SEND}_title`, "(&N)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+  [TABS_SEND]: {
+    id: TABS_SEND,
+    title: i18n.getMessage(`${TABS_SEND}_title`, "(&N)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+  */
+  "sep-2": {
+    id: "sep-2",
+    type: "separator",
+  },
+  /* all tabs */
+  [TAB_ALL_RELOAD]: {
+    id: TAB_ALL_RELOAD,
+    title: i18n.getMessage(`${TAB_ALL_RELOAD}_title`, "(&L)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TAB_ALL_SELECT]: {
+    id: TAB_ALL_SELECT,
+    title: i18n.getMessage(`${TAB_ALL_SELECT}_title`, "(&S)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TAB_ALL_BOOKMARK]: {
+    id: TAB_ALL_BOOKMARK,
+    title: i18n.getMessage(`${TAB_ALL_BOOKMARK}_title`, "(&B)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+  "sep-3": {
+    id: "sep-3",
     type: "separator",
   },
   /* tab group */
@@ -130,6 +216,7 @@ export const menuItems = {
     title: i18n.getMessage(`${TAB_GROUP}_title`, "(&G)"),
     type: "normal",
     enabled: false,
+    visible: true,
     subItems: {
       [TAB_GROUP_COLLAPSE]: {
         id: TAB_GROUP_COLLAPSE,
@@ -137,47 +224,10 @@ export const menuItems = {
         toggleTitle: i18n.getMessage(`${TAB_GROUP_EXPAND}_title`, "(&E)"),
         type: "normal",
         enabled: false,
+        visible: true,
       },
       "sepTabGroup-1": {
         id: "sepTabGroup-1",
-        type: "separator",
-      },
-      [TAB_GROUP_RELOAD]: {
-        id: TAB_GROUP_RELOAD,
-        title: i18n.getMessage(`${TAB_GROUP_RELOAD}_title`, "(&R)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_GROUP_SYNC]: {
-        id: TAB_GROUP_SYNC,
-        title: i18n.getMessage(`${TAB_GROUP_SYNC}_title`, "(&S)"),
-        type: "normal",
-        enabled: false,
-      },
-      "sepTabGroup-2": {
-        id: "sepTabGroup-2",
-        type: "separator",
-      },
-      [TAB_GROUP_PIN]: {
-        id: TAB_GROUP_PIN,
-        title: i18n.getMessage(`${TAB_GROUP_PIN}_title`, "(&P)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_GROUP_DUPE]: {
-        id: TAB_GROUP_DUPE,
-        title: i18n.getMessage(`${TAB_GROUP_DUPE}_title`, "(&D)"),
-        type: "normal",
-        enabled: false,
-      },
-      [TAB_GROUP_BOOKMARK]: {
-        id: TAB_GROUP_BOOKMARK,
-        title: i18n.getMessage(`${TAB_GROUP_BOOKMARK}_title`, "(&B)"),
-        type: "normal",
-        enabled: false,
-      },
-      "sepTabGroup-3": {
-        id: "sepTabGroup-3",
         type: "separator",
       },
       [TAB_GROUP_SELECTED]: {
@@ -185,78 +235,102 @@ export const menuItems = {
         title: i18n.getMessage(`${TAB_GROUP_SELECTED}_title`, "(&G)"),
         type: "normal",
         enabled: false,
+        visible: true,
       },
       [TAB_GROUP_DETACH]: {
         id: TAB_GROUP_DETACH,
         title: i18n.getMessage(`${TAB_GROUP_DETACH}_title`, "(&T)"),
         type: "normal",
         enabled: false,
-      },
-      "sepTabGroup-4": {
-        id: "sepTabGroup-4",
-        type: "separator",
+        visible: true,
       },
       [TAB_GROUP_UNGROUP]: {
         id: TAB_GROUP_UNGROUP,
         title: i18n.getMessage(`${TAB_GROUP_UNGROUP}_title`, "(&U)"),
         type: "normal",
         enabled: false,
-      },
-      "sepTabGroup-5": {
-        id: "sepTabGroup-5",
-        type: "separator",
-      },
-      [TAB_GROUP_CLOSE]: {
-        id: TAB_GROUP_CLOSE,
-        title: i18n.getMessage(`${TAB_GROUP_CLOSE}_title`, "(&C)"),
-        type: "normal",
-        enabled: false,
+        visible: true,
       },
     },
   },
-  "sep-2": {
-    id: "sep-2",
+  "sep-4": {
+    id: "sep-4",
     type: "separator",
   },
-  /* selected tabs */
-  // FIXME: issue #33, #38
-  /*
-  [TAB_SELECTED]: {
-  },
-  "sep-3": {
-    id: "sep-3",
-    type: "separator",
-  },
-  */
-  /* all tabs */
-  [TAB_RELOAD_ALL]: {
-    id: TAB_RELOAD_ALL,
-    title: i18n.getMessage(`${TAB_RELOAD_ALL}_title`, "(&R)"),
+  [TAB_CLOSE_OPTIONS]: {
+    id: TAB_CLOSE_OPTIONS,
+    title: i18n.getMessage(`${TAB_CLOSE_OPTIONS}_title`, "(&O)"),
     type: "normal",
     enabled: false,
+    visible: true,
+    subItems: {
+      [TAB_CLOSE_END]: {
+        id: TAB_CLOSE_END,
+        title: i18n.getMessage(`${TAB_CLOSE_END}_title`, "(&E)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      [TAB_CLOSE_OTHER]: {
+        id: TAB_CLOSE_OTHER,
+        title: i18n.getMessage(`${TABS_CLOSE_OTHER}_title`, "(&O)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      // Not implemented yet in Firefox
+      /*
+      [TAB_CLOSE_DUPE]: {
+        id: TAB_CLOSE_DUPE,
+        title: i18n.getMessage(`${TAB_CLOSE_DUPE}_title`, "(&D)"),
+        type: "normal",
+        enabled: false,
+        visible: true,
+      },
+      */
+    },
   },
-  [TAB_BOOKMARK_ALL]: {
-    id: TAB_BOOKMARK_ALL,
-    title: i18n.getMessage(`${TAB_BOOKMARK_ALL}_title`, "(&B)"),
+  [TABS_CLOSE_OTHER]: {
+    id: TABS_CLOSE_OTHER,
+    title: i18n.getMessage(`${TABS_CLOSE_OTHER}_title`, "(&O)"),
     type: "normal",
     enabled: false,
-  },
-  [TAB_SELECT_ALL]: {
-    id: TAB_SELECT_ALL,
-    title: i18n.getMessage(`${TAB_SELECT_ALL}_title`, "(&S)"),
-    type: "normal",
-    enabled: false,
-  },
-  "sep-3": {
-    id: "sep-3",
-    type: "separator",
+    visible: false,
   },
   [TAB_CLOSE_UNDO]: {
     id: TAB_CLOSE_UNDO,
     title: i18n.getMessage(`${TAB_CLOSE_UNDO}_title`, "(&U)"),
     type: "normal",
     enabled: false,
+    visible: true,
   },
+  [TAB_CLOSE]: {
+    id: TAB_CLOSE,
+    title: i18n.getMessage(`${TAB_CLOSE}_title`, "(&C)"),
+    type: "normal",
+    enabled: false,
+    visible: true,
+  },
+  [TABS_CLOSE]: {
+    id: TABS_CLOSE,
+    title: i18n.getMessage(`${TABS_CLOSE}_title`, "(&U)"),
+    type: "normal",
+    enabled: false,
+    visible: false,
+  },
+};
+
+/**
+ * get target element
+ * @param {number} id - target element ID
+ * @returns {Object} - target element
+ */
+export const getTargetElement = async id => {
+  if (!Number.isInteger(id)) {
+    throw new TypeError(`Expected Number but got ${getType(id)}`);
+  }
+  const target = await menus.getTargetElement(id);
+  return target || null;
 };
 
 /**
@@ -265,10 +339,11 @@ export const menuItems = {
  * @returns {?string|number} - menu item ID
  */
 export const createMenuItem = async (data = {}) => {
-  const {enabled, id, parentId, title, type} = data;
+  const {enabled, id, parentId, title, type, visible} = data;
   let menuItemId;
   if (isString(id)) {
-    menuItemId = await menus.create({enabled, id, parentId, title, type});
+    menuItemId =
+      await menus.create({enabled, id, parentId, title, type, visible});
   }
   return menuItemId || null;
 };
@@ -292,6 +367,7 @@ export const createContextualIdentitiesMenu = async () => {
         parentId: TAB_REOPEN_CONTAINER,
         title: name,
         type: "normal",
+        visible: true,
       };
       func.push(createMenuItem(opt));
     }
@@ -309,9 +385,9 @@ export const createContextMenu = async (menu = menuItems, parentId = null) => {
   const items = Object.keys(menu);
   const func = [];
   for (const item of items) {
-    const {enabled, id, subItems, title, type} = menu[item];
-    const itemData = {enabled, id, parentId, title, type};
-    func.push(createMenuItem(itemData));
+    const {enabled, id, subItems, title, type, visible} = menu[item];
+    const opt = {enabled, id, parentId, title, type, visible};
+    func.push(createMenuItem(opt));
     if (id === TAB_REOPEN_CONTAINER) {
       func.push(createContextualIdentitiesMenu());
     } else if (subItems) {
@@ -336,6 +412,7 @@ export const updateContextualIdentitiesMenu = async info => {
     parentId: TAB_REOPEN_CONTAINER,
     title: name,
     type: "normal",
+    visible: true,
   };
   await menus.update(cookieStoreId, opt);
 };
@@ -348,8 +425,8 @@ export const updateContextualIdentitiesMenu = async info => {
  */
 export const updateContextMenu = async (menuItemId, data = {}) => {
   if (isString(menuItemId)) {
-    const {enabled, title} = data;
-    await menus.update(menuItemId, {enabled, title});
+    const {enabled, title, visible} = data;
+    await menus.update(menuItemId, {enabled, title, visible});
   }
 };
 
