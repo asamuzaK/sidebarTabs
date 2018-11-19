6610b8c426050131051a32704d6916271105a690
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-11-20 02:02:31 +0900

    Update menu

---
+++ test/menu-items.test.js
@@ -0,0 +1,79 @@
+/**
+ * menu-items.test.js
+ */
+/* eslint-disable  max-nested-callbacks, no-await-in-loop, no-magic-numbers */
+
+import {assert} from "chai";
+import {afterEach, beforeEach, describe, it} from "mocha";
+import {browser} from "./mocha/setup.js";
+import menuItems from "../src/mjs/menu-items.js";
+import {
+  TAB_ALL_BOOKMARK, TAB_ALL_RELOAD, TAB_ALL_SELECT,
+  TAB_BOOKMARK, TAB_CLOSE, TAB_CLOSE_END, TAB_CLOSE_OTHER, TAB_CLOSE_UNDO,
+  TAB_DUPE,
+  TAB_GROUP, TAB_GROUP_COLLAPSE, TAB_GROUP_DETACH, TAB_GROUP_DETACH_TABS,
+  TAB_GROUP_SELECTED, TAB_GROUP_UNGROUP,
+  TAB_MOVE, TAB_MOVE_END, TAB_MOVE_START, TAB_MOVE_WIN, TAB_MUTE, TAB_PIN,
+  TAB_RELOAD, TAB_REOPEN_CONTAINER,
+  TABS_BOOKMARK, TABS_CLOSE, TABS_CLOSE_OTHER, TABS_DUPE, TABS_MOVE,
+  TABS_MOVE_END, TABS_MOVE_START, TABS_MOVE_WIN, TABS_MUTE, TABS_PIN,
+  TABS_RELOAD, TABS_REOPEN_CONTAINER,
+} from "../src/mjs/constant.js";
+
+describe("menu items", () => {
+  beforeEach(() => {
+    global.browser = browser;
+  });
+  afterEach(() => {
+    delete global.browser;
+  });
+
+  it("should get browser object", () => {
+    assert.isObject(browser, "browser");
+  });
+
+  describe("should get string and object", () => {
+    const itemKeys = [
+      TAB_RELOAD, TABS_RELOAD, TAB_MUTE, TABS_MUTE, "sep-1",
+      TAB_PIN, TABS_PIN, TAB_BOOKMARK, TABS_BOOKMARK, TAB_DUPE, TABS_DUPE,
+      TAB_REOPEN_CONTAINER, TABS_REOPEN_CONTAINER, TAB_MOVE, TABS_MOVE, "sep-2",
+      TAB_ALL_RELOAD, TAB_ALL_SELECT, TAB_ALL_BOOKMARK, "sep-3",
+      TAB_GROUP, "sep-4", TAB_CLOSE_END, TAB_CLOSE_OTHER, TABS_CLOSE_OTHER,
+      TAB_CLOSE_UNDO, TAB_CLOSE, TABS_CLOSE,
+    ];
+    const items = Object.entries(menuItems);
+
+    it("should get equal length", () => {
+      assert.isTrue(items.length === itemKeys.length, "length");
+    });
+
+    it("should get string and object", () => {
+      for (const [key, value] of items) {
+        assert.isTrue(itemKeys.includes(key), "item");
+        assert.isString(key, "key");
+        assert.isObject(value, "value");
+      }
+    });
+  });
+
+  describe("sub items", () => {
+    const parentItemKeys = [TAB_MOVE, TABS_MOVE, TAB_GROUP];
+    const subItemKeys = [
+      TAB_MOVE_START, TAB_MOVE_END, TAB_MOVE_WIN,
+      TABS_MOVE_START, TABS_MOVE_END, TABS_MOVE_WIN,
+      TAB_GROUP_COLLAPSE, "sepTabGroup-1", TAB_GROUP_SELECTED,
+      TAB_GROUP_DETACH, TAB_GROUP_DETACH_TABS, TAB_GROUP_UNGROUP,
+    ];
+
+    it("should get string and object of sub items", () => {
+      parentItemKeys.forEach(itemKey => {
+        const items = Object.entries(menuItems[itemKey].subItems);
+        for (const [key, value] of items) {
+          assert.isTrue(subItemKeys.includes(key), "item");
+          assert.isString(key, "key");
+          assert.isObject(value, "value");
+        }
+      });
+    });
+  });
+});
---
+++ test/menu.test.js
@@ -0,0 +1,377 @@
+/**
+ * menu.test.js
+ */
+/* eslint-disable  max-nested-callbacks, no-await-in-loop, no-magic-numbers */
+
+import {JSDOM} from "jsdom";
+import {assert} from "chai";
+import {afterEach, beforeEach, describe, it} from "mocha";
+import sinon from "sinon";
+import {browser} from "./mocha/setup.js";
+import * as mjs from "../src/mjs/menu.js";
+
+describe("menu", () => {
+  /**
+   * create jsdom
+   * @returns {Object} - jsdom instance
+   */
+  const createJsdom = () => {
+    const domstr = "<!DOCTYPE html><html><head></head><body></body></html>";
+    const opt = {
+      runScripts: "dangerously",
+    };
+    return new JSDOM(domstr, opt);
+  };
+  let window, document;
+  beforeEach(() => {
+    const dom = createJsdom();
+    window = dom && dom.window;
+    document = window && window.document;
+    global.browser = browser;
+    global.window = window;
+    global.document = document;
+  });
+  afterEach(() => {
+    window = null;
+    document = null;
+    delete global.browser;
+    delete global.window;
+    delete global.document;
+  });
+
+  it("should get browser object", () => {
+    assert.isObject(browser, "browser");
+  });
+
+  describe("create context menu item", () => {
+    const func = mjs.createMenuItem;
+
+    it("should not call function if no argument given", async () => {
+      const i = browser.menus.create.callCount;
+      const res = await func();
+      assert.strictEqual(browser.menus.create.callCount, i, "not called");
+      assert.isNull(res, "result");
+    });
+
+    it("should not call function if argument is empty object", async () => {
+      const i = browser.menus.create.callCount;
+      const res = await func({});
+      assert.strictEqual(browser.menus.create.callCount, i, "not called");
+      assert.isNull(res, "result");
+    });
+
+    it("should not call function if object does not contain id", async () => {
+      const i = browser.menus.create.callCount;
+      const res = await func({
+        foo: "bar",
+      });
+      assert.strictEqual(browser.menus.create.callCount, i, "not called");
+      assert.isNull(res, "result");
+    });
+
+    it("should not call function if id is not string", async () => {
+      const i = browser.menus.create.callCount;
+      const res = await func({
+        id: [],
+      });
+      assert.strictEqual(browser.menus.create.callCount, i, "not called");
+      assert.isNull(res, "result");
+    });
+
+    it("should call function", async () => {
+      browser.menus.create.resolves("foo");
+      const i = browser.menus.create.callCount;
+      const res = await func({
+        id: "foo",
+      });
+      assert.strictEqual(browser.menus.create.callCount, i + 1, "called");
+      assert.strictEqual(res, "foo", "result");
+      browser.menus.create.flush();
+    });
+  });
+
+  describe("create contextual identities menu", () => {
+    const func = mjs.createContextualIdentitiesMenu;
+
+    it("should get empty array", async () => {
+      browser.contextualIdentities.query.withArgs({}).resolves(null);
+      const i = browser.contextualIdentities.query.callCount;
+      const j = browser.menus.create.callCount;
+      const res = await func();
+      assert.strictEqual(browser.contextualIdentities.query.callCount, i + 1,
+                         "called");
+      assert.strictEqual(browser.menus.create.callCount, j, "not called");
+      assert.deepEqual(res, [], "result");
+    });
+
+    it("should get array", async () => {
+      browser.contextualIdentities.query.withArgs({}).resolves([
+        {
+          cookieStoreId: "foo",
+        },
+        {
+          cookieStoreId: "bar",
+        },
+      ]);
+      const i = browser.contextualIdentities.query.callCount;
+      const j = browser.menus.create.callCount;
+      const res = await func();
+      assert.strictEqual(browser.contextualIdentities.query.callCount, i + 1,
+                         "called");
+      assert.strictEqual(browser.menus.create.callCount, j + 2, "called");
+      assert.deepEqual(res, [null, null], "result");
+    });
+  });
+
+  describe("create context menu", () => {
+    const func = mjs.createContextMenu;
+
+    it("should get array", async () => {
+      const res = await func();
+      assert.isTrue(res.length > 0, "result");
+    });
+  });
+
+  describe("update contextual identities menu", () => {
+    const func = mjs.updateContextualIdentitiesMenu;
+
+    it("should not call function", async () => {
+      const i = browser.menus.update.callCount;
+      await func();
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should not call function if argument is empty object", async () => {
+      const i = browser.menus.update.callCount;
+      await func({});
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should throw if color not contained", async () => {
+      await func({
+        foo: "bar",
+      }).catch(e => {
+        assert.strictEqual(e.message, "Expected String but got Undefined.",
+                           "throw");
+      });
+    });
+
+    it("should throw if cookieStoreId not contained", async () => {
+      await func({
+        color: "red",
+      }).catch(e => {
+        assert.strictEqual(e.message, "Expected String but got Undefined.",
+                           "throw");
+      });
+    });
+
+    it("should throw if icon not contained", async () => {
+      await func({
+        color: "red",
+        cookieStoreId: "foo",
+      }).catch(e => {
+        assert.strictEqual(e.message, "Expected String but got Undefined.",
+                           "throw");
+      });
+    });
+
+    it("should throw if name not contained", async () => {
+      await func({
+        color: "red",
+        cookieStoreId: "foo",
+        icon: "fingerprint",
+      }).catch(e => {
+        assert.strictEqual(e.message, "Expected String but got Undefined.",
+                           "throw");
+      });
+    });
+
+    it("should call function", async () => {
+      const i = browser.menus.update.callCount;
+      await func({
+        color: "red",
+        cookieStoreId: "foo",
+        icon: "fingerprint",
+        name: "bar",
+      });
+      assert.strictEqual(browser.menus.update.callCount, i + 1, "called");
+    });
+  });
+
+  describe("update context menu", () => {
+    const func = mjs.updateContextMenu;
+
+    it("should not call function if 1st arg is not given", async () => {
+      const i = browser.menus.update.callCount;
+      await func();
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should not call function if 1st arg is not string", async () => {
+      const i = browser.menus.update.callCount;
+      await func(1);
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should not call function if 2nd arg is not given", async () => {
+      const i = browser.menus.update.callCount;
+      await func("foo");
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should not call function if 2nd arg is empty object", async () => {
+      const i = browser.menus.update.callCount;
+      await func("foo", {});
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should not call function if prop does not match", async () => {
+      const i = browser.menus.update.callCount;
+      await func("foo", {
+        bar: "baz",
+      });
+      assert.strictEqual(browser.menus.update.callCount, i, "not called");
+    });
+
+    it("should call function", async () => {
+      const i = browser.menus.update.callCount;
+      await func("foo", {
+        enabled: true,
+      });
+      assert.strictEqual(browser.menus.update.callCount, i + 1, "called");
+    });
+  });
+
+  describe("remove contextual identities menu", () => {
+    const func = mjs.removeContextualIdentitiesMenu;
+
+    it("should not call function if no argument given", async () => {
+      const i = browser.menus.remove.callCount;
+      await func();
+      assert.strictEqual(browser.menus.remove.callCount, i, "not called");
+    });
+
+    it("should not call function if argument is empty object", async () => {
+      const i = browser.menus.remove.callCount;
+      await func({});
+      assert.strictEqual(browser.menus.remove.callCount, i, "not called");
+    });
+
+    it("should not call function if cookieStoreId not contained", async () => {
+      const i = browser.menus.remove.callCount;
+      await func({
+        foo: "bar",
+      });
+      assert.strictEqual(browser.menus.remove.callCount, i, "not called");
+    });
+
+    it("should call function", async () => {
+      const i = browser.menus.remove.callCount;
+      await func({
+        cookieStoreId: "foo",
+      });
+      assert.strictEqual(browser.menus.remove.callCount, i + 1, "called");
+    });
+  });
+
+  describe("override context menu", () => {
+    const func = mjs.overrideContextMenu;
+    beforeEach(() => {
+      if (typeof browser.menus.overrideContext !== "function") {
+        browser.menus.overrideContext = sinon.stub();
+      }
+    });
+
+    it("should call function with empty object argument", async () => {
+      const i = browser.menus.overrideContext.withArgs({}).callCount;
+      await func({});
+      assert.strictEqual(browser.menus.overrideContext.withArgs({}).callCount,
+                         i + 1, "called");
+    });
+
+    it("should call function with object argument", async () => {
+      const i = browser.menus.overrideContext.withArgs({
+        tabId: 1,
+        context: "tab",
+      }).callCount;
+      const elm = document.createElement("p");
+      const body = document.querySelector("body");
+      elm.dataset.tabId = "1";
+      body.appendChild(elm);
+      await func({
+        target: elm,
+      });
+      assert.strictEqual(browser.menus.overrideContext.withArgs({
+        tabId: 1,
+        context: "tab",
+      }).callCount, i + 1, "called");
+    });
+
+    it("should call function with empty object argument", async () => {
+      const i = browser.menus.overrideContext.withArgs({}).callCount;
+      const elm = document.createElement("p");
+      const body = document.querySelector("body");
+      elm.dataset.tabId = browser.tabs.TAB_ID_NONE;
+      body.appendChild(elm);
+      await func({
+        target: elm,
+      });
+      assert.strictEqual(browser.menus.overrideContext.withArgs({}).callCount,
+                         i + 1, "called");
+    });
+  });
+
+  describe("handle contextmenu click", () => {
+    const func = mjs.contextmenuOnClick;
+    beforeEach(() => {
+      if (typeof browser.menus.overrideContext !== "function") {
+        browser.menus.overrideContext = sinon.stub();
+      }
+    });
+
+    it("should call function", async () => {
+      const i = browser.menus.overrideContext.callCount;
+      await func({});
+      assert.strictEqual(browser.menus.overrideContext.callCount, i + 1,
+                         "called");
+    });
+  });
+
+  describe("handle contextualIdentities.onCreated", () => {
+    const func = mjs.contextualIdentitiesOnCreated;
+
+    it("should call function", async () => {
+      const i = browser.contextualIdentities.query.callCount;
+      await func();
+      assert.strictEqual(browser.contextualIdentities.query.callCount, i + 1,
+                         "called");
+    });
+  });
+
+  describe("handle contextualIdentities.onRemoved", () => {
+    const func = mjs.contextualIdentitiesOnRemoved;
+
+    it("should call function", async () => {
+      const i = browser.menus.remove.callCount;
+      await func({
+        cookieStoreId: "foo",
+      });
+      assert.strictEqual(browser.menus.remove.callCount, i + 1, "called");
+    });
+  });
+
+  describe("handle contextualIdentities.onUpdated", () => {
+    const func = mjs.contextualIdentitiesOnUpdated;
+
+    it("should call function", async () => {
+      const i = browser.menus.update.callCount;
+      await func({
+        color: "red",
+        cookieStoreId: "foo",
+        icon: "fingerprint",
+        name: "bar",
+      });
+      assert.strictEqual(browser.menus.update.callCount, i + 1, "called");
+    });
+  });
+});
