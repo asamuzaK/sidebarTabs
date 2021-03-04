bd6dcbc9c5d672ed8cfd52ee6eb9b55d46e3ab54
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2021-03-05 02:07:00 +0900

    Rename `Close Tabs to the Bottom` (#120)
    Fix #118

--- src/mjs/menu-items.js
+++ src/mjs/menu-items.js
@@ -5,7 +5,7 @@
 /* shared */
 import {
   NEW_TAB_OPEN_CONTAINER, TAB_ALL_BOOKMARK, TAB_ALL_RELOAD, TAB_ALL_SELECT,
-  TAB_BOOKMARK, TAB_CLOSE, TAB_CLOSE_END, TAB_CLOSE_OTHER, TAB_CLOSE_UNDO,
+  TAB_BOOKMARK, TAB_CLOSE, TAB_CLOSE_BOTTOM, TAB_CLOSE_OTHER, TAB_CLOSE_UNDO,
   TAB_DUPE,
   TAB_GROUP, TAB_GROUP_COLLAPSE, TAB_GROUP_COLLAPSE_OTHER, TAB_GROUP_CONTAINER,
   TAB_GROUP_DETACH, TAB_GROUP_DETACH_TABS, TAB_GROUP_DOMAIN, TAB_GROUP_EXPAND,
@@ -418,9 +418,9 @@ export default {
     enabled: true,
     visible: true,
     subItems: {
-      [TAB_CLOSE_END]: {
-        id: TAB_CLOSE_END,
-        title: i18n.getMessage(`${TAB_CLOSE_END}_title`, '(&E)'),
+      [TAB_CLOSE_BOTTOM]: {
+        id: TAB_CLOSE_BOTTOM,
+        title: i18n.getMessage(`${TAB_CLOSE_BOTTOM}_title`, '(&B)'),
         type: 'normal',
         contexts: ['tab'],
         viewTypes: ['sidebar'],
