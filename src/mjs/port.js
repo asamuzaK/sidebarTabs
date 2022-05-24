ff4d33451124ad393911d671d4b88cbfd427e6ba
asamuzaK (Kazz) <kazz@asamuzak.jp>
2022-05-24 21:54:42 +0900

    Update port handler

--- src/mjs/port.js
+++ src/mjs/port.js
@@ -2,6 +2,7 @@
  * port.js
  */
 
+import { logErr, throwErr } from './common.js';
 import { getCurrentWindow, makeConnection } from './browser.js';
 import { SIDEBAR } from './constant.js';
 
@@ -23,26 +24,57 @@ export const ports = new Map();
 export const removePort = async portId => ports.delete(portId);
 
 /**
+ * port on disconnect
+ *
+ * @param {object} port - runtime.Port
+ * @returns {Function} - promise chain
+ */
+export const portOnDisconnect = (port = {}) => {
+  const { error, name: portId } = port;
+  error && logErr(error);
+  return removePort(portId).catch(throwErr);
+};
+
+/**
  * add port
  *
+ * @param {string} portId - port ID
  * @returns {object} - runtime.Port
  */
-export const addPort = async () => {
+export const addPort = async portId => {
   const win = await getCurrentWindow({
     populate: true
   });
   const { id: windowId } = win;
   let port;
   if (windowId !== WINDOW_ID_NONE) {
-    const portId = `${SIDEBAR}_${windowId}`;
+    portId ??= `${SIDEBAR}_${windowId}`;
     if (ports.has(portId)) {
       port = ports.get(portId);
     } else {
       port = await makeConnection({
         name: portId
       });
+      port.onDisconnect.addListener(portOnDisconnect);
       ports.set(portId, port);
     }
+  }
+  return port || null;
+};
+
+/**
+ * get port
+ *
+ * @param {string} portId - port ID
+ * @param {boolean} add - add port if port does not exist
+ * @returns {object} - runtime.Port
+ */
+export const getPort = async (portId, add = false) => {
+  let port;
+  if (portId && ports.has(portId)) {
+    port = ports.get(portId);
+  } else if (add) {
+    port = await addPort(portId);
   }
   return port || null;
 };
--- test/port.test.js
+++ test/port.test.js
@@ -7,6 +7,7 @@ import { assert } from 'chai';
 import { afterEach, beforeEach, describe, it } from 'mocha';
 import { browser, mockPort } from './mocha/setup.js';
 import { SIDEBAR } from '../src/mjs/constant.js';
+import sinon from 'sinon';
 
 /* test */
 import * as mjs from '../src/mjs/port.js';
@@ -60,6 +61,57 @@ describe('port', () => {
     });
   });
 
+  describe('port on disconnect', () => {
+    const func = mjs.portOnDisconnect;
+    beforeEach(() => {
+      mjs.ports.clear();
+    });
+    afterEach(() => {
+      mjs.ports.clear();
+    });
+
+    it('should get false', async () => {
+      const res = await func();
+      assert.isFalse(res, 'result');
+    });
+
+    it('should get false', async () => {
+      mjs.ports.set('foo', {});
+      const res = await func({
+        name: 'bar'
+      });
+      assert.strictEqual(mjs.ports.size, 1, 'size');
+      assert.isFalse(res, 'result');
+    });
+
+    it('should get true', async () => {
+      const stubError = sinon.stub(console, 'error');
+      mjs.ports.set('foo', {});
+      const res = await func({
+        name: 'foo'
+      });
+      const { called: errCalled } = stubError;
+      stubError.restore();
+      assert.isFalse(errCalled, 'error called');
+      assert.strictEqual(mjs.ports.size, 0, 'size');
+      assert.isTrue(res, 'result');
+    });
+
+    it('should log error', async () => {
+      const stubError = sinon.stub(console, 'error');
+      mjs.ports.set('foo', {});
+      const res = await func({
+        error: new Error('error'),
+        name: 'foo'
+      });
+      const { calledOnce: errCalled } = stubError;
+      stubError.restore();
+      assert.isTrue(errCalled, 'error called');
+      assert.strictEqual(mjs.ports.size, 0, 'size');
+      assert.isTrue(res, 'result');
+    });
+  });
+
   describe('add port', () => {
     const func = mjs.addPort;
     beforeEach(() => {
@@ -99,11 +151,115 @@ describe('port', () => {
         id: 1
       });
       const stubConnect = browser.runtime.connect;
+      const res = await func(`${SIDEBAR}_1`);
+      assert.isTrue(stubCurrentWin.calledOnce, 'called current windows');
+      assert.isTrue(stubConnect.calledOnce, 'called connect');
+      assert.strictEqual(mjs.ports.size, 1, 'size');
+      assert.isObject(res, 'result');
+      assert.strictEqual(res.name, `${SIDEBAR}_1`, 'name');
+    });
+
+    it('should get port object', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: 1
+      });
+      const stubConnect = browser.runtime.connect;
       mjs.ports.set(`${SIDEBAR}_1`, {
         name: `${SIDEBAR}_1`
       });
       const res = await func();
       assert.isTrue(stubCurrentWin.calledOnce, 'called current windows');
+      assert.isFalse(stubConnect.called, 'not called connect');
+      assert.strictEqual(mjs.ports.size, 1, 'size');
+      assert.isObject(res, 'result');
+      assert.strictEqual(res.name, `${SIDEBAR}_1`, 'name');
+    });
+
+    it('should get port object', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: 1
+      });
+      const stubConnect = browser.runtime.connect;
+      mjs.ports.set(`${SIDEBAR}_1`, {
+        name: `${SIDEBAR}_1`
+      });
+      const res = await func(`${SIDEBAR}_1`);
+      assert.isTrue(stubCurrentWin.calledOnce, 'called current windows');
+      assert.isFalse(stubConnect.called, 'not called connect');
+      assert.strictEqual(mjs.ports.size, 1, 'size');
+      assert.isObject(res, 'result');
+      assert.strictEqual(res.name, `${SIDEBAR}_1`, 'name');
+    });
+  });
+
+  describe('get port', () => {
+    const func = mjs.getPort;
+    beforeEach(() => {
+      mjs.ports.clear();
+    });
+    afterEach(() => {
+      mjs.ports.clear();
+    });
+
+    it('should get null', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: browser.windows.WINDOW_ID_NONE
+      });
+      const stubConnect = browser.runtime.connect;
+      const res = await func();
+      assert.isFalse(stubCurrentWin.calledOnce, 'not called current windows');
+      assert.isFalse(stubConnect.called, 'not called connect');
+      assert.strictEqual(mjs.ports.size, 0, 'size');
+      assert.isNull(res, 'result');
+    });
+
+    it('should get null', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: browser.windows.WINDOW_ID_NONE
+      });
+      const stubConnect = browser.runtime.connect;
+      const res = await func(`${SIDEBAR}_1`, true);
+      assert.isTrue(stubCurrentWin.calledOnce, 'called current windows');
+      assert.isFalse(stubConnect.called, 'not called connect');
+      assert.strictEqual(mjs.ports.size, 0, 'size');
+      assert.isNull(res, 'result');
+    });
+
+    it('should get null', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: 1
+      });
+      const stubConnect = browser.runtime.connect;
+      const res = await func(`${SIDEBAR}_1`);
+      assert.isFalse(stubCurrentWin.calledOnce, 'not called current windows');
+      assert.isFalse(stubConnect.calledOnce, 'not called connect');
+      assert.strictEqual(mjs.ports.size, 0, 'size');
+      assert.isNull(res, 'result');
+    });
+
+    it('should get port object', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: 1
+      });
+      const stubConnect = browser.runtime.connect;
+      const res = await func(`${SIDEBAR}_1`, true);
+      assert.isTrue(stubCurrentWin.calledOnce, 'called current windows');
+      assert.isTrue(stubConnect.calledOnce, 'called connect');
+      assert.strictEqual(mjs.ports.size, 1, 'size');
+      assert.isObject(res, 'result');
+      assert.strictEqual(res.name, `${SIDEBAR}_1`, 'name');
+    });
+
+    it('should get port object', async () => {
+      const stubCurrentWin = browser.windows.getCurrent.resolves({
+        id: 1
+      });
+      const stubConnect = browser.runtime.connect;
+      mjs.ports.set(`${SIDEBAR}_1`, {
+        name: `${SIDEBAR}_1`
+      });
+      const res = await func(`${SIDEBAR}_1`);
+      assert.isFalse(stubCurrentWin.calledOnce, 'not called current windows');
       assert.isFalse(stubConnect.called, 'not called connect');
       assert.strictEqual(mjs.ports.size, 1, 'size');
       assert.isObject(res, 'result');
