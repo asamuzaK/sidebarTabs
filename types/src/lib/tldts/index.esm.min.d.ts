f6c6dc6c4ce3ef318b93b25cf76151b8577e09e9
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-04-04 09:23:28 +0900

    Update libs

--- types/src/lib/tldts/index.esm.min.d.ts
+++ types/src/lib/tldts/index.esm.min.d.ts
@@ -1,7 +1,7 @@
 declare function d(n: any, t?: {}): any;
-declare function m(n: any, t?: {}): any;
-declare function f(n: any, t?: {}): any;
+declare function x(n: any, t?: {}): any;
 declare function c(n: any, t?: {}): any;
 declare function h(n: any, t?: {}): any;
-declare function s(n: any, t?: {}): any;
-export { d as getDomain, m as getDomainWithoutSuffix, f as getHostname, c as getPublicSuffix, h as getSubdomain, s as parse };
+declare function m(n: any, t?: {}): any;
+declare function f(n: any, t?: {}): any;
+export { d as getDomain, x as getDomainWithoutSuffix, c as getHostname, h as getPublicSuffix, m as getSubdomain, f as parse };
