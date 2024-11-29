e82059a62fb4104139a12fe6954fcf1b160fa054
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-11-29 22:52:57 +0900

    Update types

--- types/src/mjs/color.d.ts
+++ types/src/mjs/color.d.ts
@@ -1,3 +1,3 @@
 export function convertRgbToHex(rgb: Array<number>): string;
 export function getColorInHex(value: string, opt?: object): (string | any[]) | null;
-export function compositeLayeredColors(overlay: string, base: string): string | null;
+export function compositeLayeredColors(overlay: string, base: string): string;
