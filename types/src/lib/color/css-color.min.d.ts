345cd148c62219f1e1071ff8ad6e7f2e43dbcd72
asamuzaK (Kazz) <kazz@asamuzak.jp>
2025-05-27 09:45:55 +0900

    v16.0.4

--- types/src/lib/color/css-color.min.d.ts
+++ types/src/lib/color/css-color.min.d.ts
@@ -1,42 +1,42 @@
-declare namespace C1 {
-    export { m1 as colorToHex };
-    export { g1 as colorToHsl };
-    export { v1 as colorToHwb };
-    export { b1 as colorToLab };
-    export { w1 as colorToLch };
-    export { $1 as colorToOklab };
-    export { y1 as colorToOklch };
-    export { N1 as colorToRgb };
+declare namespace F1 {
+    export { v1 as colorToHex };
+    export { b1 as colorToHsl };
+    export { w1 as colorToHwb };
+    export { $1 as colorToLab };
+    export { y1 as colorToLch };
+    export { N1 as colorToOklab };
+    export { E1 as colorToOklch };
+    export { C1 as colorToRgb };
     export { rl as colorToXyz };
-    export { E1 as colorToXyzD50 };
-    export { d1 as numberToHex };
+    export { k1 as colorToXyzD50 };
+    export { g1 as numberToHex };
 }
-declare function F1(t: any, e?: {}): any;
-declare function k1(t: any, e?: {}): boolean;
+declare function S1(t: any, e?: {}): any;
+declare function x1(t: any, e?: {}): boolean;
 declare function A0(t: any, e?: {}): any;
 declare namespace Qa {
     export { Gr as cssCalc };
     export { xu as cssVar };
-    export { H0 as extractDashedIdent };
+    export { z0 as extractDashedIdent };
     export { fn as isColor };
-    export { u1 as isGradient };
+    export { f1 as isGradient };
     export { Ii as splitValue };
 }
-declare function m1(t: any, e?: {}): any;
-declare function g1(t: any, e?: {}): any;
 declare function v1(t: any, e?: {}): any;
 declare function b1(t: any, e?: {}): any;
 declare function w1(t: any, e?: {}): any;
 declare function $1(t: any, e?: {}): any;
 declare function y1(t: any, e?: {}): any;
 declare function N1(t: any, e?: {}): any;
-declare function rl(t: any, e?: {}): any;
 declare function E1(t: any, e?: {}): any;
-declare function d1(t: any): any;
+declare function C1(t: any, e?: {}): any;
+declare function rl(t: any, e?: {}): any;
+declare function k1(t: any, e?: {}): any;
+declare function g1(t: any): any;
 declare function Gr(t: any, e?: {}): any;
 declare function xu(t: any, e?: {}): string | String;
-declare function H0(t: any): any;
+declare function z0(t: any): any;
 declare function fn(t: any, e?: {}): boolean;
-declare function u1(t: any, e?: {}): boolean;
-declare function Ii(t: any, e?: string): any;
-export { C1 as convert, F1 as cssCalc, k1 as isColor, A0 as resolve, Qa as utils };
+declare function f1(t: any, e?: {}): boolean;
+declare function Ii(t: any, e?: {}): any;
+export { F1 as convert, S1 as cssCalc, x1 as isColor, A0 as resolve, Qa as utils };
